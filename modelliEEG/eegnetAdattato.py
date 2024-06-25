95058d8bef9e758783166fbebd01e6b7d528c058
Antonio Pio Caruso <86917486+Luffy65@users.noreply.github.com>
2024-06-25 13:03:31 +0200

    Add files via upload

--- modelliEEG/eegnetAdattato.py
+++ modelliEEG/eegnetAdattato.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class EEGNet(nn.Module):
     """
-    four block:
+    Four block:
     1. conv2d
     2. depthwiseconv2d
     3. separableconv2d
@@ -16,8 +16,8 @@ class EEGNet(nn.Module):
         self.block1 = nn.Sequential()
         self.block1_conv = nn.Conv2d(in_channels=1,
                                      out_channels=8,
-                                     kernel_size=(1, 128),
-                                     padding=(0, 64),
+                                     kernel_size=(1, 256),
+                                     padding=(0, 128),
                                      bias=False
                                      )
         self.block1.add_module('conv1', self.block1_conv)
@@ -25,14 +25,14 @@ class EEGNet(nn.Module):
 
         # 2. depthwiseconv2d
         self.block2 = nn.Sequential()
-        # [N, 8, 32, 1152] -> [N, 16, 1, 1152]
+        # [N, 8, 32, 1280] -> [N, 16, 1, 1280]
         self.block2.add_module('conv2', nn.Conv2d(in_channels=8,
                                                   out_channels=16,
                                                   kernel_size=(32, 1),
                                                   groups=2,
                                                   bias=False))
         self.block2.add_module('act1', nn.ELU())
-        # [N, 16, 1, 1152] -> [N, 16, 1, 288]
+        # [N, 16, 1, 1280] -> [N, 16, 1, 320]
         self.block2.add_module('pool1', nn.AvgPool2d(kernel_size=(1, 4)))
         self.block2.add_module('drop1', nn.Dropout(p=0.5))
 
@@ -58,30 +58,28 @@ class EEGNet(nn.Module):
         self.classify = nn.Sequential(nn.Linear(576, num_class))
 
     def forward(self, x):
-        # [B, 32, 1152] -> [B, 1, 32, 1152]
-        x = torch.reshape(x, (self.batch_size, 1, 32, 1152))
+        # [B, 32, 1280] -> [B, 1, 32, 1280]
+        x = torch.reshape(x, (x.size(0), 1, 32, 1280))
 
-        # [B, 1, 32, 1152] -> [B, 1, 32, 1151]
-        x = x[:, :, :, range(1151)]
+        # Remove one column before convolution if needed
+        x = x[:, :, :, :1279]
 
-        # [B, 1, 32, 1151] -> [B, 8, 32, 1152]
+        # [B, 1, 32, 1279] -> [B, 8, 32, 1280]
         x = self.block1(x)
 
-        # [B, 8, 32, 1152] -> [B, 16, 1, 1152] -> [B, 16, 1, 288]
+        # [B, 8, 32, 1280] -> [B, 16, 1, 1280] -> [B, 16, 1, 320]
         x = self.block2(x)
 
-        # [B, 16, 1, 288] -> [B, 16, 1, 287]
-        x = x[:, :, :, range(287)]
+        # [B, 16, 1, 320] -> [B, 16, 1, 319]
+        x = x[:, :, :, :319]
 
-        # [B, 16, 1, 287] -> [B, 16, 1, 36]
+        # [B, 16, 1, 319] -> [B, 16, 1, 40]
         x = self.block3(x)
 
-        # [B, 16, 1, 36] -> [B, 576]
+        # [B, 16, 1, 40] -> [B, 640]
         x = x.view(x.size(0), -1)
 
-        # [B, 576] -> [B, num_class]
+        # [B, 640] -> [B, num_class]
         x = self.classify(x)
-
-        # x = nn.functional.softmax(x, dim=1)
 
         return x
